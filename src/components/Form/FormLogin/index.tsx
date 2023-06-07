4280de8db37f6c0fa8b121907a223e86a2693d70
Diego Guilherme <diegoguilherme752@oultook.com>
2023-06-07 05:39:34 -0300

    feat: created style login page

--- src/components/Form/FormLogin/index.tsx
+++ src/components/Form/FormLogin/index.tsx
@@ -1,13 +1,14 @@
 "use client";
 
+import React from "react";
+import { useClient } from "@/hooks/useClient";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { LoginData, loginSchema } from "@/schemas/client.schema";
+
 import Button from "@/components/Button";
 import Input from "@/components/Input";
 import InputError from "@/components/Input/InputError";
-import { useClient } from "@/hooks/useClient";
-import { LoginData, loginSchema } from "@/schemas/client.schema";
-import { zodResolver } from "@hookform/resolvers/zod";
-import React, { ReactNode } from "react";
-import { useForm } from "react-hook-form";
 
 const FormLogin = () => {
   const { handleLogin } = useClient();
@@ -28,23 +29,24 @@ const FormLogin = () => {
       onSubmit={handleSubmit(handleFormSubmit)}
       noValidate
       autoComplete="off"
-      className="flex flex-col gap-4"
+      className="flex flex-col gap-5"
     >
       <Input
         type="email"
-        inputName="email"
+        label="E-mail"
         placeholder="Digite seu e-mail"
+        error={errors?.email && errors.email.message!}
         register={register("email")}
       />
-      {errors?.email && <InputError message={errors.email.message!} />}
       <Input
         type="password"
-        inputName="password"
+        label="Senha"
+        error={errors?.password && errors.password.message!}
         placeholder="Digite aqui sua senha"
         register={register("password")}
       />
-      {errors?.password && <InputError message={errors.password.message!} />}
-      <Button typeBtn="submit">Entrar</Button>
+      <h6 className="h7 text-sky-800 text-end mb-4 cursor-pointer">Esqueceu sua senha?</h6>
+      <Button type={"brand"} submit>Entrar</Button>
     </form>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,13 +7,19 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
       colors: {
-        modal: "rgba(0, 0, 0, 0.5)"
+        modal: "rgba(0, 0, 0, 0.5)",
+      },
+      fontSize: {
+        xs: [".75rem", "1rem"],
+        sm: [".875rem", "1.25rem"],
+        base: ["1rem", "1.5rem"],
+        lg: ["1.25rem", "1.75rem"],
+        xl: ["1.5rem", "2rem"],
+        '2xl': ["1.75rem", "2.25rem"],
+        '3xl': ["2rem", "2.5rem"],
+        '4xl': ["2.25rem", "2.75rem"],
+        '5xl': ["2.75rem", "3rem"],
       }
     },
   },
